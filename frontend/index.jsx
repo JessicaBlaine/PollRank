bbce9e4fed2f75fb3701dbe685ccee772deb1b6d
Work <joshuablaine@users.noreply.github.com>
2016-10-12 19:45:16 -0700

    initial frontend skeleton

---
+++ frontend/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+
+const Router = ReactRouter.Router;
+const Route = ReactRouter.Route;
+const IndexRoute = ReactRouter.IndexRoute;
+const hashHistory = ReactRouter.hashHistory;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<div>test</div>, rootEl);
+});
