505d6876f53b7c3c1c15c89b156b2538826be66a
Work <joshuablaine@users.noreply.github.com>
2016-11-01 15:55:47 -0700

    updateOption action

--- frontend/actions/option_actions.js
+++ frontend/actions/option_actions.js
@@ -1,13 +1,18 @@
 export const CREATE_OPTION = 'CREATE_OPTION';
 export const REMOVE_OPTION = 'REMOVE_OPTION';
+export const UPDATE_OPTION = 'UPDATE_OPTION';
 
 export const createOption = option => ({
   type: CREATE_OPTION,
   option,
 });
 
-
 export const removeOption = option => ({
   type: REMOVE_OPTION,
+  option,
+});
+
+export const updateOption = option => ({
+  type: UPDATE_OPTION,
   option,
 });
