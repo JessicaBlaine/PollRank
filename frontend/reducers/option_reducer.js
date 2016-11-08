4f6b848a22ffc4d1a88a3f112bb1a067108b2c3e
Work <joshuablaine@users.noreply.github.com>
2016-11-07 19:01:52 -0800

    options reducer

--- frontend/reducers/option_reducer.js
+++ frontend/reducers/option_reducer.js
@@ -1,3 +1,21 @@
 import merge from 'lodash/merge';
 
-import {} from '../actions/option_actions';
+import { CREATE_OPTION,
+         REMOVE_OPTION,
+         UPDATE_OPTION } from '../actions/option_actions';
+
+const optionReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case CREATE_OPTION:
+      return state;
+    case REMOVE_OPTION:
+      return state;
+    case UPDATE_OPTION:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default optionReducer;
