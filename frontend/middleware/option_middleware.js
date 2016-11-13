6abc10ab9ec0afc6aefda715c56ec00e65b14a3c
Work <joshuablaine@users.noreply.github.com>
2016-11-13 13:40:43 -0800

    option middleware edit

--- frontend/middleware/option_middleware.js
+++ frontend/middleware/option_middleware.js
@@ -3,6 +3,7 @@
 export default ({ getState, dispatch }) => next => action => {
   const optionSuccess = data => dispatch(receivePoll(data));
 
+
   switch (action.type) {
     default:
       return next(action);
