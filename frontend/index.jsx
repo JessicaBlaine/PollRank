0a5c30d8bada64d91fd9d7f40956b727258f40d8
Work <joshuablaine@users.noreply.github.com>
2016-12-30 00:32:31 -0500

    chenges

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -7,6 +7,8 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store = configureStore();
