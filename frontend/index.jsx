ac227f3438ee3c67a8d7649f3e257160f564f280
Work <joshuablaine@users.noreply.github.com>
2017-01-08 20:00:52 -0500

    change

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ReactRouter, { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 
+
 import Root from './components/root';
 import configureStore from './store/store';
 
